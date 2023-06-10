1b5cbee2509d1988390d936c72052fb2ed647e78
Dario Alarcon <darioalarcon832@gmail.com>
2023-06-09 20:51:30 -0500

    base de datos basica con socket

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const { Socket } = require('socket.io');
+require('dotenv').config();
+
+const app = express();//app de express
+
+const server = require('http').createServer(app);//node server
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket');
+
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
+
+server.listen(process.env.PORT, (err)=>{
+    
+    if (err) throw new Error(err);
+    console.log('Servidor corriendo en puerto', process.env.PORT);
+}
+);
---
+++ sockets/socket.js
@@ -0,0 +1,14 @@
+const {io} = require('../index');
+
+io.on('connection', client => {//socket comunication
+    
+    console.log('cliente conectado')
+    client.on('disconnect', () => { 
+        console.log('cliente desconectado')
+    });
+
+    client.on('mensaje', (payload)=>{
+        console.log('mensaje', payload);
+        io.emit('mensaje',{admin: 'new message'})
+    });
+});
